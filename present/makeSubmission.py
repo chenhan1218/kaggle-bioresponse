b0011bce782f923130488a3685918c1bb023e3e6
swem <r97922120@ntu.edu.tw>
2014-02-16 23:50:09 +0800

    first submission with RandomForestClassifier
    from tutorial https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience

---
+++ present/makeSubmission.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+import sys
+
+def main():
+    training_file_name = sys.argv[1]
+    testing_file_name = sys.argv[2]
+
+    #create the training & test sets, skipping the header row with [1:]
+    dataset = genfromtxt(open(training_file_name,'r'), delimiter=',', dtype='f8')[1:]    
+    target = [x[0] for x in dataset]
+    train = [x[1:] for x in dataset]
+    test = genfromtxt(open(testing_file_name,'r'), delimiter=',', dtype='f8')[1:]
+
+    #create and train the random forest
+    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    rf = RandomForestClassifier(n_estimators=100)
+    rf.fit(train, target)
+    predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
+
+    savetxt(sys.stdout, predicted_probs, delimiter=',', fmt='%d,%f', 
+            header='MoleculeId,PredictedProbability', comments = '')
+
+if __name__=="__main__":
+    main()
